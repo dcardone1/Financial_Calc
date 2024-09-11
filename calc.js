ee2ba3b1e173141dde66f0a559efe8054b4623be
dcardone1 <dcardone@gmail.com>
2024-09-10 23:55:21 -0300

    Se agregaron más fórmulas y la logica de los radio buttons y check boxes

--- calc.js
+++ calc.js
@@ -1,4 +1,11 @@
+function init(){
+    document.getElementById("R_r").disabled = true;
+    document.getElementById("VF_r").checked = true;
+    document.getElementById("anualidad_ordinaria").checked = true;
 
+    showFormula()
+
+}
 
 function valorFinal() {
     var VP = document.getElementById("VP").value;
@@ -16,16 +23,65 @@ function valorPresente() {
     document.getElementById("VF").value = VF.toFixed(2);
 }
 
+//show the corresponding formula according to the selected calculation
 function showFormula() {
-    var r  = document.getElementById("VP_r");
-    if (r.checked) {
-        document.getElementById("equation_paragraph").innerHTML = "$$VP = VF\over \(1+i)^n$$";
-        MathJax.typeset();
+    var checkBoxAnualidad = document.getElementById("anualidad")
+    if (!checkBoxAnualidad.checked){
+        document.getElementById("R_r").disabled = true
+        var r  = document.getElementById("VP_r");
+        if (r.checked) {
+            document.getElementById("equation_paragraph").innerHTML = "$$VP ={ VF\\over\(1 + i)^n}$$";
+            MathJax.typeset();
+        }
+        r = document.getElementById("VF_r");
+        if (r.checked) {
+            document.getElementById("equation_paragraph").innerHTML = "$$VF = VP(1+i)^n$$";
+            MathJax.typeset();
+        }
+        r = document.getElementById("i_r");
+        if (r.checked) {
+            document.getElementById("equation_paragraph").innerHTML = "$$i = \\sqrt[n]{VF\\over\ VP}-1$$";
+            MathJax.typeset();
+        }
+        r = document.getElementById("n_r");
+        if (r.checked) {
+            document.getElementById("equation_paragraph").innerHTML = "$$n = {ln({VF\\over\ VP})\\over\{ln(1+i)}}$$";
+            MathJax.typeset();
+        }
     }
-    r = document.getElementById("VF_r");
-    if (r.checked) {
-        document.getElementById("equation_paragraph").innerHTML = "$$VF = VP(1+i)^n$$";
-        MathJax.typeset();
+    else if (document.getElementById("anualidad_ordinaria").checked){
+        document.getElementById("R_r").disabled = false
+        var r = document.getElementById("VP_r");
+        if (r.checked) {
+            document.getElementById("equation_paragraph").innerHTML = 
+            "$$VP = R\\Big[{\\sum_{t=1}^n {1\\over\ {(1+i)^t}}}\\Big] = R\\Big[{1-{1\\over\ {(1+i)^n}}\\over\ i}\\Big]$$";
+            MathJax.typeset();
+        }
+        r = document.getElementById("VF_r");
+        if (r.checked) {
+            document.getElementById("equation_paragraph").innerHTML = 
+            "$$VF = R\\Big[{\\sum_{t=1}^n {(1+i)^{n-t}}}\\Big] = R\\Big[{{(1+i)^n-1}\\over\ i}\\Big]$$";
+            MathJax.typeset();
+        }
+        r = document.getElementById("i_r");
+        if (r.checked) {
+            document.getElementById("equation_paragraph").innerHTML = "$$i$$";
+            MathJax.typeset();
+        }
+        r = document.getElementById("n_r");
+        if (r.checked) {
+            document.getElementById("equation_paragraph").innerHTML = "$$n$$";
+            MathJax.typeset();
+        }
+        r = document.getElementById("R_r");
+        if (r.checked) {
+            document.getElementById("equation_paragraph").innerHTML = "$$R$$";
+            MathJax.typeset();
+        }
+    }
+    else {
+
     }
 }
 
+window.addEventListener("load", init);
