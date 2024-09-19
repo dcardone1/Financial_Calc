c106844a7235db1e25c008b6dcc7517584aacc3d
dcardone1 <dcardone@gmail.com>
2024-09-19 00:31:11 -0300

    Se agregan todas las fórmulas excepto la del interes en anualidades

--- calc.js
+++ calc.js
@@ -1,12 +1,12 @@
 function calcular (){
 
-    var VP = document.getElementById("VP").value;
-    var VF = document.getElementById("VF").value;
-    var i = document.getElementById("i").value;
-    var n = document.getElementById("periods").value;
-    var R = document.getElementById("R").value;
+    var VP = parseFloat(document.getElementById("VP").value);
+    var VF = parseFloat(document.getElementById("VF").value);
+    var i = parseFloat(document.getElementById("i").value);
+    var n = parseFloat(document.getElementById("periods").value);
+    var R = parseFloat(document.getElementById("R").value);
 
-    document.getElementById(result_id).value = formula(VP, VF, i, n, R)
+    document.getElementById(result_id).value = formula(VP, VF, i, n, R);
 }
 
 function init(){
@@ -14,6 +14,7 @@ function init(){
     document.getElementById("VF_r").checked = true;
     document.getElementById("anualidad_ordinaria").checked = true;
     document.getElementById("con_VP").checked = true;
+    document.getElementById("i").value=0.01;
     var formula = function () {
         return 0;
     }
@@ -33,34 +34,34 @@ function showFormula() {
             MathJax.typeset();
             result_id="VP"
             formula = function (VP, VF, i, n, R) {
-                return (VF / Math.pow(1 + parseFloat(i), n)).toFixed(2);
+                return (VF / Math.pow(1 + i, n)).toFixed(2);
             }
         }
         r = document.getElementById("VF_r");
         if (r.checked) {
             document.getElementById("equation_paragraph").innerHTML = "$$VF = VP(1+i)^n$$";
             MathJax.typeset();
-            result_id = "VF"
+            result_id = "VF";
             formula = function (VP, VF, i, n, R) {
-                return (VP*Math.pow(1 + parseFloat(i), n)).toFixed(2);
+                return (VP*Math.pow(1 + i, n)).toFixed(2);
             }
         }
         r = document.getElementById("i_r");
         if (r.checked) {
             document.getElementById("equation_paragraph").innerHTML = "$$i = \\sqrt[n]{VF\\over\ VP}-1$$";
             MathJax.typeset();
-            result_id = "i"
+            result_id = "i";
             formula = function (VP, VF, i, n, R) {
-                return (Math.pow(parseFloat(VF)/parseFloat(VP), 1.0/n)-1).toFixed(6);
+                return (Math.pow(VF/VP, 1.0/n)-1).toFixed(8);
             }
         }
         r = document.getElementById("n_r");
         if (r.checked) {
             document.getElementById("equation_paragraph").innerHTML = "$$n = {ln({VF\\over\ VP})\\over\{ln(1+i)}}$$";
             MathJax.typeset();
-            result_id = "periods"
+            result_id = "periods";
             formula = function (VP, VF, i, n, R) {
-                return (Math.log(parseFloat(VF)/parseFloat(VP))/Math.log(1+parseFloat(i))).toFixed(2);
+                return Math.round(Math.log(VF/VP)/Math.log(1+i), 2);
             }
         }
     }
@@ -71,12 +72,20 @@ function showFormula() {
             document.getElementById("equation_paragraph").innerHTML = 
             "$$VP = R\\Big[{\\sum_{t=1}^n {1\\over\ {(1+i)^t}}}\\Big] = R\\Big[{1-{1\\over\ {(1+i)^n}}\\over\ i}\\Big]$$";
             MathJax.typeset();
+            result_id = "VP";
+            formula = function (VP, VF, i, n, R) {
+                return (R*(1-1/Math.pow(1+i, n))/i).toFixed(2);
+            }
         }
         r = document.getElementById("VF_r");
         if (r.checked) {
             document.getElementById("equation_paragraph").innerHTML = 
             "$$VF = R\\Big[{\\sum_{t=1}^n {(1+i)^{n-t}}}\\Big] = R\\Big[{{(1+i)^n-1}\\over\ i}\\Big]$$";
             MathJax.typeset();
+            result_id = "VF";
+            formula = function (VP, VF, i, n, R) {
+                return (R * (Math.pow(1 + i, n)-1) / i).toFixed(2);
+            }
         }
         if(document.getElementById("con_VP").checked){
             r = document.getElementById("i_r");
@@ -90,12 +99,20 @@ function showFormula() {
                 document.getElementById("equation_paragraph").innerHTML = 
                 "$$n={{ln({{R}\\over\ {R - VPi}})}\\over\ {ln(1+i)}}$$";
                 MathJax.typeset();
+                result_id = "periods";
+                formula = function (VP, VF, i, n, R) {
+                    return Math.round((Math.log(R/(R-VP*i))/Math.log(1+i)), 2);
+                }
             }
             r = document.getElementById("R_r");
             if (r.checked) {
                 document.getElementById("equation_paragraph").innerHTML =
                  "$$R = {{VPi}\\over\ {\\Big[{1-{1\\over\ {(1+i)^n}}}\\Big]}}$$";
                 MathJax.typeset();
+                result_id = "R";
+                formula = function (VP, VF, i, n, R) {
+                    return (VP*i/(1-1/Math.pow(1+i,n))).toFixed(2);
+                }
             }
         }
         else {
@@ -110,12 +127,20 @@ function showFormula() {
                 document.getElementById("equation_paragraph").innerHTML = 
                 "$$n={{ln \\Big[{{VFi + R}\\over\ R}\\Big]}\\over\ {ln(1+i)}}$$";
                 MathJax.typeset();
+                result_id = "periods";
+                formula = function (VP, VF, i, n, R) {
+                    return Math.round((Math.log((VF*i+R)/R)/Math.log(1+i)), 2);
+                }
             }
             r = document.getElementById("R_r");
             if (r.checked) {
                 document.getElementById("equation_paragraph").innerHTML = 
                 "$$R = {VFi\\over\ {\\Big[{(1+i)^n-1}\\Big]}}$$";
                 MathJax.typeset();
+                result_id = "R";
+                formula = function (VP, VF, i, n, R) {
+                    return (VF*i/(Math.pow(1+i, n)-1)).toFixed(2);
+                }
             }
         }
         
@@ -127,12 +152,20 @@ function showFormula() {
             document.getElementById("equation_paragraph").innerHTML =
                 "$$VP = R\\Big[{\\sum_{t=1}^n {1\\over\ {(1+i)^{t-1}}}}\\Big] = R(1+i)\\Big[{1-{1\\over\ {(1+i)^n}}\\over\ i}\\Big]$$";
             MathJax.typeset();
+            result_id = "VP";
+            formula = function (VP, VF, i, n, R) {
+                return (R*(1+i)*(1 - 1 / Math.pow(1 + i, n)) / i).toFixed(2);
+            }
         }
         r = document.getElementById("VF_r");
         if (r.checked) {
             document.getElementById("equation_paragraph").innerHTML =
                 "$$VF = R\\Big[{\\sum_{t=1}^n {(1+i)^{n+1-t}}}\\Big] = R(1+i)\\Big[{{(1+i)^n-1}\\over\ i}\\Big]$$";
             MathJax.typeset();
+            result_id = "VF";
+            formula = function (VP, VF, i, n, R) {
+                return (R*(1+i)*(Math.pow(1 + i, n) - 1) / i).toFixed(2);
+            }
         }
         if (document.getElementById("con_VP").checked) {
             r = document.getElementById("i_r");
@@ -146,12 +179,20 @@ function showFormula() {
                 document.getElementById("equation_paragraph").innerHTML = 
                 "$$n={{ln \\Big[{{R(1+i)}\\over\ {R(1+i)-VPi}} \\Big]}\\over\ {ln(1+i)}}$$";
                 MathJax.typeset();
+                result_id = "periods";
+                formula = function (VP, VF, i, n, R) {
+                    return Math.round((Math.log(R*(1+i) / (R*(1+i) - VP * i)) / Math.log(1 + i)), 2);
+                }
             }
             r = document.getElementById("R_r");
             if (r.checked) {
                 document.getElementById("equation_paragraph").innerHTML = 
                 "$$R={VP\\over\ {(1+i)\\Big[{1-{1\\over\ {(1+i)^n}}\\over\ i}\\Big]}}$$";
                 MathJax.typeset();
+                result_id = "R";
+                formula = function (VP, VF, i, n, R) {
+                    return (VP*i/((1+i)*(1-1/Math.pow(1+i, n)))).toFixed(2);
+                }
             }
         }
         else {
@@ -166,12 +207,20 @@ function showFormula() {
                 document.getElementById("equation_paragraph").innerHTML = 
                 "$$n={{ln\\Big[{{VFi + R(1+i)}\\over\ {R(1+i)}}\\Big]}\\over\ {ln(1+i)}}$$";
                 MathJax.typeset();
+                result_id = "periods";
+                formula = function (VP, VF, i, n, R) {
+                    return Math.round((Math.log((VF*i+R*(1+i))/(R*(1+i))) / Math.log(1 + i)), 2);
+                }
             }
             r = document.getElementById("R_r");
             if (r.checked) {
                 document.getElementById("equation_paragraph").innerHTML = 
                 "$$R={VF \\over\ {(1+i)\\Big[{{(1+i)^n-1}\\over\ i}\\Big]}}$$";
                 MathJax.typeset();
+                result_id = "R";
+                formula = function (VP, VF, i, n, R) {
+                    return (VF*i/((1+i)*(Math.pow(1+i, n)-1))).toFixed(2);
+                }
             }
         }
     }
